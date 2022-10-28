a644b1ed8ff380d9f22b1f52c2e334b8dffee44c
samarth.narasimha <samarth.narasimha@skillassure.com>
2022-10-28 11:56:53 +0530

    final commit

---
+++ src/main/java/com/training/ProductAppSecurityBatch1AuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAppSecurityBatch1AuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductAppSecurityBatch1AuthDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<h1> Hi Welcome to galaxe </h1>
+<a href="addProduct">Add Products</a>|
+<a href="viewAllProducts">View All Products</a>|
+
+</body>
+</html>
